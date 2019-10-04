67ad3d9ad9e46ae51a8773e3e4f9511f35d19cb1
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-04 02:33:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,5 +28,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  
+  hash = [key]
+  if shipping_manifest["top hat"]
+  shipping_manifest["top hat"] += 1
+else
+  shipping_manifest["top hat"] = 1
+end
 end
