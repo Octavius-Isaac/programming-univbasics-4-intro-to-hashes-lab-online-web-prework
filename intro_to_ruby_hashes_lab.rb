878e277673788f2d13243edd09de6986fe657b16
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-04 02:17:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  def read_from_hash(hash, key)
   hash = {:key => "Hi" }
   hash[:key]
   hash[:negative]
-end
 end
 
 def update_counting_hash(hash, key)
